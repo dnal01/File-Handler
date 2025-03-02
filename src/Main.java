3590f48f832670c8275a9aad9d840b17a2221f01
Denys Nalbat <denysnalbat@icloud.com>
2025-03-02 10:01:35 +0100

    Rebase 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("My Super app!");
-        System.out.println("App Version 2.0");
+        System.out.println("Regby!");
+        System.out.println("App Version 3.0");
         System.out.println("Calculator method");
     }
 }
