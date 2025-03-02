e2be1bf0a2b1659bf2094fe1d8d60131d34882a1
Denys Nalbat <denysnalbat@icloud.com>
2025-03-02 10:00:42 +0100

    Update app name to Supper

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Calculator");
+        System.out.println("Calculator Supper!");
         System.out.println("App Version 1.0");
     }
 }
