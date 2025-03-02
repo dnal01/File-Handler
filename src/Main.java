5539fb8b689037d7989522ce645b78ec4b9d5590
Denys Nalbat <denysnalbat@icloud.com>
2025-03-02 09:58:19 +0100

    Update app name

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("My Super app!");
+        System.out.println("Calculator");
         System.out.println("App Version 1.0");
     }
 }
