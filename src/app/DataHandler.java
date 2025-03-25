8a13b73237f25f76df94119adcafa9b817cd6b04
dnal01 <143934392+dnal01@users.noreply.github.com>
2025-03-25 08:28:21 +0100

    Update DataHandler.java

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -1,18 +1,30 @@
 package app;
 
-import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class DataHandler {
 
-    // Метод формує виведення імені за певним індексом
-    public String formOutput(List<String> list, int index) {
-        try {
-            String name = list.get(index);
-            return "Name: " + name + " is in index " + index;
-        } catch (IndexOutOfBoundsException e) {
-            return "Wrong index!";
-        }
+   Map<Integer, String> map = new DataRepository();
+
+    // Метод формує виведення нумерованого переліку імен
+    public String getAll() {
+        StringBuilder sb = new StringBuilder();
+        AtomicInteger count = new AtomicInteger(0);
+        map.forEach((id, name) ->
+                sb.append(String.format("%d) %d, %s%n",
+                        count.incrementAndGet(),id, name)
+                ));
+        return "\\nALL NAMES:\\n" + sb;
+    }
+
+    // Метод формує виведення імені за певним id
+    public String getById(int id) {
+        if (coKey(id)) {
+            return "\\nNAME: id " + id + ", " +
+                    map.put(id);
+        } else return "No data!";
+    }
     }
 
     // Метод формує виведення нумерованого списку імен
