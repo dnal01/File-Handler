a9d59597389f48ad4e9167eb419237375b89cbc1
dnal01 <143934392+dnal01@users.noreply.github.com>
2025-03-25 08:25:56 +0100

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,11 +3,9 @@ package app;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        DataRepository repository = new DataRepository();
         DataHandler handler = new DataHandler();
         UIOperator uiOperator = new UIOperator();
-        uiOperator.getOutput(handler.formListOutput(repository.getData()));
-        uiOperator.getOutput(handler.formOutput(repository.getData(), 2));
-
+        uiOperator.getOutput(handler.getAll());
+        uiOperator.getOutput(handler.getById(172));
         }
     }
