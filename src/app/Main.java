442de7e3b4c76487452c62090854e620a33d8ce4
Denys Nalbat <denysnalbat@icloud.com>
2025-04-22 19:18:27 +0200

    update Shop app

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,14 +2,10 @@ package app;
 
 import app.controller.ClothController;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class Main {
 
     public static void main(String[] args) {
         ClothController clothController = new ClothController();
+        clothController.appStart();
     }
 }
